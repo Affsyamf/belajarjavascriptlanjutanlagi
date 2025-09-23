be544abb618633a9a764d5578287ca6b4d00523b
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-23 19:22:02 +0700

    object creeate

--- 1.js
+++ 1.js
@@ -18,27 +18,62 @@
 
 //2.  function declaration
 
+const methodMahasiswa = {
+    makan: function (porsi) {
+        this.energi += porsi;
+        console.log(`halo ${this.nama} selamat mkn`);
+    },
+
+    laper: function (min) {
+        this.energi -= min;
+        console.log(`laper ${this.nama}`);
+    },
+
+    tetap: function (sama) {
+        this.energi == sama;
+        console.log(`sama ${this.energi}`);
+    },
+
+    tidur: function (min) {
+        this.energi += min * 2;
+        console.log(`hallo ${this.nama} tidur kali 2`)
+
+    }
+};
+
 function Mahasiswa (nama, energi) {
-    let mahasiswa = {};
+    let mahasiswa = Object.create(methodMahasiswa);
     mahasiswa.energi = energi;
     mahasiswa.nama = nama;
 
-    mahasiswa.makan = function (porsi) {
-        this.energi += porsi;
-        console.log(`halo ${this.nama} selamat mkn`);
-    }
-
-    mahasiswa.laper = function (laper) {
-        this.energi -= laper;
-        console.log(`laper ${this.nama}`)
-    }
-
-    mahasiswa.tetap = function (tetap) {
-        this.energi == tetap;
-        console.log(`sama ${this.energi}`)
-    }
     return mahasiswa;
 }
 
 let afif = Mahasiswa ('Afif', 10);
 let misel = Mahasiswa ('misel', 10);
+
+
+
+//3. constractor function
+
+// function Mahasiswa (nama, energi) {
+//     this.energi = energi;
+//     this.nama = nama;
+
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`halo ${this.nama} selamat mkn`);
+//     }
+
+//     this.laper = function (laper) {
+//         this.energi -= laper;
+//         console.log(`laper ${this.nama}`)
+//     }
+
+//     this.tetap = function (tetap) {
+//         this.energi == tetap;
+//         console.log(`sama ${this.energi}`)
+//     }
+// }
+
+// let afif = new Mahasiswa('afif', 10);

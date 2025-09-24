2fb9c691761a6f49f9867d8c3bb4a0bd7cfd4ea5
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-25 04:42:32 +0700

    belajar function dan block scope

---
+++ closure.js
@@ -0,0 +1,82 @@
+// function salam(waktu) {
+//     return function(nama) {
+//         console.log (`halo ${nama} selamat ${waktu}`);
+//     }
+// }
+
+// let pagi = salam('pagi');
+// let siang = salam('siang');
+// let malam = salam ('malam');
+
+// pagi ('afif');
+
+ 
+// let add = (function () {
+//     let counter = 0;
+//     return function () {
+//         return ++ counter;
+//     }
+// }) ();
+
+// // let a = add();
+
+// counter = 100;
+
+// console.log(add());
+// console.log(add());
+// console.log(add());
+// console.log(add());
+
+
+
+function panggilNama () {
+    if (true) {
+    var nama = 'afif';
+    let nama2 = 'doko';
+    console.log(nama2);
+   }
+   console.log(nama);
+
+}
+
+panggilNama();
+// if (true) {
+//     var nama = 'afif'
+//     let nama2 = 'doko';
+//     console.log(nama2);
+// }
+
+// console.log(nama);
+
+
+
+// if(true) {
+//     var x = 'misel';
+//     let y = 'doko';
+// }
+// console.log(x);
+// console.log(y);
+// panggilNama();
+
+// console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function init () {
+//     // let nama = 'afif';
+//    return function tampilNama(nama) {
+//         console.log(nama);
+//     }
+// }
+// let panggilNama = init();
+// panggilNama('dika');

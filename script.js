11d673fdb9d41c93a6faf1de34ea39095429cf58
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-24 20:26:24 +0700

    hoisting, scope

---
+++ script.js
@@ -0,0 +1,54 @@
+// 2.1 excution context
+
+// let nama = 'afif';
+// let umur = 21;
+// console.log(sayHello());
+
+// function sayHello () {
+//     return `halo nama saya ${nama} umur saya ${umur}`;
+// }
+
+// var nama = 'afif';
+// var ig = "@affsyamf";
+
+// function cetakIg() {
+//     var instagramUrl = 'https://instagram.com.affsyamf ';
+//     return instagramUrl + ig;
+// }
+
+// console.log(cetakIg(ig));
+
+// function a () {
+//     console.log('ini a');
+
+//     function b () {
+//         console.log('ini b');
+
+//         function c () {
+//             console.log('ini c');
+//         }
+//         c();
+//     }
+//     b();
+// }
+
+// a();
+
+// tes
+
+function satu(){
+    var nama = 'afif';
+    console.log(nama);
+}
+
+function dua() { // dua('kontol') dari line 53 melihat tidak ada yang menerima maka turun ke lokal
+    //lokal
+    console.log(nama); //line 53 cek ke lokal karna tidak ada var/let/const yang ada hanya konsol nama
+                        // dicari di lokal tidak ada nama, maka cari ke argumen (lokal parameter) tidak ada maka cari ke global
+}
+
+console.log(nama); //jika line 51 memakai let harus di tuker posisi inisialisasi
+var nama = 'misel';
+satu();
+dua('ddy');
+console.log(nama);

497dc7b5e094cb3d73065b764634a89df8ca3599
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-26 13:07:26 +0700

    loteral template

---
+++ literals.js
@@ -0,0 +1,37 @@
+const nama = 'afif';
+// const umur = 21;
+// const nim = 6702223011;
+// console.log(`nama saya ${nama}, umur saya ${umur}, nim saya ${nim}`);
+
+
+//embeded ekspresion
+// console.log(`${1+1}, ${2*2}`);
+// console.log(`Hati hati ada ${alert(nama)}`);
+
+// const x = 10;
+// console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil' }`);
+
+//html fragments
+
+// const mhs = {
+//     nama: 'afif',
+//     nim: 6702223011,
+// };
+
+// const el = `<div class="mhs">
+// <h2>${mhs.nama}</h2>
+// <span class="nim">${mhs.nim}</span>
+// </div>`;
+
+// console.log(el);
+
+const mhs = 
+    {
+        nama: 'afif',
+        nim: 6702223011
+    }
+;
+
+console.table(mhs);
+
+// document.body.innerHTML = el;

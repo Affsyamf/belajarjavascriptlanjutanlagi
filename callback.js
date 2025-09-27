58a128c4defd7518b233c99db85eb41c6adc6876
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-28 03:44:16 +0700

    callback sederhana

---
+++ callback.js
@@ -0,0 +1,48 @@
+//callback
+//sychronus callback
+// function halo(nama){
+//     alert(`hallo ${nama}`)
+// }
+
+// function pesan(callback){
+//     const na = prompt('masukan nama : ')
+//     callback(na)
+// }
+// pesan(na => alert(`halo ${na}`))
+
+// const mhs = [{
+//     "nama": 'afif',
+//     'nim': 3011
+// },
+// {
+//     'nama': 'misel',
+//     'nim': 3044
+// }
+// ]
+
+// mhs.forEach(m => console.log(m.nama))
+
+
+
+//asychronus callback
+
+console.log('mulai')
+setTimeout (() => {
+
+    console.log('selesai')
+}, 2000)
+
+console.log('tugas')
+
+
+// function mhs (nama, callback) {
+//     console.log(`nama saya ${nama}`)
+//     callback()
+// }
+
+
+// function lanjut (){
+//     console.log(`oke lanjut ${(mhs.nama)}`)
+// }
+
+// mhs('afif', lanjut);

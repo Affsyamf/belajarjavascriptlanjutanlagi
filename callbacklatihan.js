8b4a20fb2f810c79177e8971e443db29d7d3873a
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-29 06:48:22 +0700

    card work

--- callbacklatihan.js
+++ callbacklatihan.js
@@ -10,12 +10,44 @@ $.ajax({
                     <div class="card-body">
                         <h5 class="card-title">${m.Title}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                        <a href="#" class="btn btn-primary">Show Details</a>
+                        <a href="#" class="btn btn-primary detail-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid=${m.imdbID}>Show Details</a>
                     </div>
                     </div>
             </div>`;
         });
         $('.movie-container').html(cards);
+
+        //tombol di klik
+        $('.detail-btn').on('click', function() {
+            $.ajax({
+                url: 'http://www.omdbapi.com/?apikey=540941ba&i=' + $(this).data('imdbid'),
+                success: m => {
+                    const movieDetail = `<div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="${m.Poster}" class="img-fluid" alt="">
+                </div>
+                <div class="col-md">
+                    <ul class="list-group">
+                <li class="list-group-item"><h4>${m.Tittle} (${m.Year})</h4></li>
+                <li class="list-group-item"><strong>Director :</strong>${m.Director}</li>
+                <li class="list-group-item"><h4>Actors : </h4>${m.Actors}</li>
+                <li class="list-group-item"><h4><strong>Genre : </strong> </h4>${m.Genre}</li>
+                <li class="list-group-item"><strong>Plot : </strong><br>${m.Plot}</li>
+                </ul>
+                </div>
+            </div>
+        </div>`;
+
+        $('.modal-body').html(movieDetail)
+                },
+                error: (e) => {
+                    console.log(e.responseText)
+                }
+            })
+        });
+
+
     },
     error: (e) => {
         console.log(e.responseText)

ee2902b1aafca5d5f7f91b4beb2895943e872057
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-30 05:15:28 +0700

    fetch refactor

--- callbacklatihan.js
+++ callbacklatihan.js
@@ -38,49 +38,86 @@
 
 
 //fetch
+// const searchButton = document.querySelector('.search-btn')
+// searchButton.addEventListener('click', function(){
+//     const inputKeyword = document.querySelector('.input-keyword')
+//     fetch('http://www.omdbapi.com/?apikey=540941ba&S=' + inputKeyword.value)
+//      .then(response => response.json())
+//      .then(response => {
+//         const movies = response.Search;
+//         let cards = '';
+//         movies.forEach(m => cards += showcards(m))
+//         const movieContainer = document.querySelector('.movie-container') 
+//         movieContainer.innerHTML = cards;
+
+
+
+//         //ketika tombol detail klik
+//         const detailButton = document.querySelectorAll('.detail-btn');
+//         detailButton.forEach(btn => {
+//             btn.addEventListener('click', function(){
+//                 const imdbid = this.dataset.imdbid;
+//                 fetch('http://www.omdbapi.com/?apikey=540941ba&i=' + imdbid) 
+//                 .then(response => response.json())
+//                 .then(m => {
+//                     const movieDetail = showMovieDetail(m);
+//                     const modalBody = document.querySelector('.modal-body');
+//                     modalBody.innerHTML = movieDetail;
+//                 })
+//             })
+//         })
+
+//     })
+
+// })
+
+
+
+
+//menggunakan fetch refactor async await
+
 const searchButton = document.querySelector('.search-btn')
-searchButton.addEventListener('click', function(){
-
+searchButton.addEventListener('click', async function(){
     const inputKeyword = document.querySelector('.input-keyword')
-    fetch('http://www.omdbapi.com/?apikey=540941ba&S=' + inputKeyword.value)
-     .then(response => response.json())
-     .then(response => {
-        const movies = response.Search;
-        let cards = '';
-        movies.forEach(m => cards += showcards(m))
-        const movieContainer = document.querySelector('.movie-container') 
-        movieContainer.innerHTML = cards;
-
-        //ketika tombol detail klik
-        const detailButton = document.querySelectorAll('.detail-btn');
-        detailButton.forEach(btn => {
-            btn.addEventListener('click', function(){
-                const imdbid = this.dataset.imdbid;
-                fetch('http://www.omdbapi.com/?apikey=540941ba&i=' + imdbid) 
-                .then(response => response.json())
-                .then(m => {
-                    const movieDetail = showMovieDetail(m);
-                    const modalBody = document.querySelector('.modal-body');
-                    modalBody.innerHTML = movieDetail;
-                })
-            })
-        })
-
-    })
-
+   const movies = await getMovies(inputKeyword.value)
+   updateUI(movies)
 })
 
 
+//event binding
+document.addEventListener('click', async function(e){
+    if(e.target.classList.contains('detail-btn')) {
+        const imdbid = e.target.dataset.imdbid
+        const movieDetail = await getMovieDetail(imdbid)
+        updateUIDetail(movieDetail)
+    }
+})
 
+function getMovieDetail (imdbid) {
+  return  fetch('http://www.omdbapi.com/?apikey=540941ba&i=' + imdbid) 
+    .then(response => response.json())
+    .then(m => m)                   
+}
 
+function updateUIDetail (m) {
+     const movieDetail = showMovieDetail(m);
+     const modalBody = document.querySelector('.modal-body');
+     modalBody.innerHTML = movieDetail;
+}
 
+function getMovies(keyword) {
+    return fetch('http://www.omdbapi.com/?apikey=540941ba&S=' + keyword)
+     .then(response => response.json())
+     .then(response => response.Search)
+       
+}
 
-
-
-
-
-
-
+function updateUI (movies) {
+     let cards = '';
+     movies.forEach(m => cards += showcards(m))
+     const movieContainer = document.querySelector('.movie-container') 
+     movieContainer.innerHTML = cards;
+}
 
 function showcards (m) {
     return `<div class="col-md-4 my-3">

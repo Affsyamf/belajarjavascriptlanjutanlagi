9bbcb43764e81224c6f27915d6086c997693c7c3
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-09-29 07:01:19 +0700

    search bar

--- callbacklatihan.js
+++ callbacklatihan.js
@@ -1,19 +1,11 @@
-$.ajax({
-    url: 'http://www.omdbapi.com/?apikey=540941ba&S=Avengers',
+$('.search-btn').on('click', function(){
+    $.ajax({
+    url: 'http://www.omdbapi.com/?apikey=540941ba&S=' + $('.input-keyword').val(),
     success: results => {
         const movies = results.Search;
         let cards = '';
         movies.forEach(m => {
-            cards += `<div class="col-md-4 my-3">
-                <div class="card">
-                    <img src="${m.Poster}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">${m.Title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                        <a href="#" class="btn btn-primary detail-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid=${m.imdbID}>Show Details</a>
-                    </div>
-                    </div>
-            </div>`;
+            cards += showcards(m);
         });
         $('.movie-container').html(cards);
 
@@ -22,22 +14,7 @@ $.ajax({
             $.ajax({
                 url: 'http://www.omdbapi.com/?apikey=540941ba&i=' + $(this).data('imdbid'),
                 success: m => {
-                    const movieDetail = `<div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="${m.Poster}" class="img-fluid" alt="">
-                </div>
-                <div class="col-md">
-                    <ul class="list-group">
-                <li class="list-group-item"><h4>${m.Tittle} (${m.Year})</h4></li>
-                <li class="list-group-item"><strong>Director :</strong>${m.Director}</li>
-                <li class="list-group-item"><h4>Actors : </h4>${m.Actors}</li>
-                <li class="list-group-item"><h4><strong>Genre : </strong> </h4>${m.Genre}</li>
-                <li class="list-group-item"><strong>Plot : </strong><br>${m.Plot}</li>
-                </ul>
-                </div>
-            </div>
-        </div>`;
+                    const movieDetail = showMovieDetail(m);
 
         $('.modal-body').html(movieDetail)
                 },
@@ -53,3 +30,39 @@ $.ajax({
         console.log(e.responseText)
     }
 });
+})
+
+
+
+
+function showcards (m) {
+    return `<div class="col-md-4 my-3">
+                <div class="card">
+                    <img src="${m.Poster}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${m.Title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                        <a href="#" class="btn btn-primary detail-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid=${m.imdbID}>Show Details</a>
+                    </div>
+                    </div>
+            </div>`;
+}
+
+function showMovieDetail(m) {
+    return `<div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="${m.Poster}" class="img-fluid" alt="">
+                </div>
+                <div class="col-md">
+                    <ul class="list-group">
+                <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                <li class="list-group-item"><strong>Director :</strong>${m.Director}</li>
+                <li class="list-group-item"><h4>Actors : </h4>${m.Actors}</li>
+                <li class="list-group-item"><h4><strong>Genre : </strong> </h4>${m.Genre}</li>
+                <li class="list-group-item"><strong>Plot : </strong><br>${m.Plot}</li>
+                </ul>
+                </div>
+            </div>
+        </div>`;
+}
